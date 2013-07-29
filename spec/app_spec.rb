609222464e0e3b559c6f37e348671d6adbbe2ac6
Bryan Robles <cyborboyx@gmail.com>
2013-07-28 21:25:43 -0700

    refactor tests a tiny bit. added test for email helper method and test for correct error if JSON data is not sent

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,32 +2,62 @@ require File.expand_path('../spec_helper', __FILE__)
 
 describe app do
 
-	let(:demo_json){ { to: "John G <jmondo@newrelic.com>", subject: "hello world", body: "Hi John! Sending you an email via this awesome API I just made on the interwebs." }.to_json }
+	let(:demo_json){ { to: "bryanaka0@gmail.com", subject: "hello world", body: "Hi John! Sending you an email via this awesome API I just made on the interwebs." }.to_json }
 	let(:json_format){ 'application/json;charset=utf-8' }
+	let(:error_405){ { errors: [{ message: 'This resource only responses to a POST request. Please refer to the documentation' }] }.to_json }
+	let(:error_400){ { errors: [{ message: 'This resource requires you post JSON data with the "to", "subject", and "body" attributes.' }] }.to_json }
 
 	it "should not serve json to anything not under api" do
 		get '/hello'
+
 		last_response.should be_ok
 		last_response.content_type.should_not eq(json_format)
 	end
 
-	context "New Relic Mailer Endpoint 'api/nr_mailer' " do
+	it "#email helper method sends email" do
+		email_data = JSON.parse(demo_json)
+		email(email_data).should be_true
+	end
 
-		it "should respond to a get request with a 405 error" do
-			get '/api/nr_mailer'
+	describe "New Relic Mailer Endpoint 'api/nr_mailer' " do
 
-			last_response.status.should eq(405)
-			last_response.content_type.should eq(json_format)
-			last_response.body.should eq( {errors: [{ message: 'This resource only responses to a POST request. Please refer to the documentation' }] }.to_json )
+		context "has a malformed request" do
+
+			it "should respond to a get request with a 405 error" do
+				get '/api/nr_mailer'
+
+				last_response.status.should eq(405)
+				last_response.content_type.should eq(json_format)
+				last_response.body.should eq( error_405 )
+			end
+	
+			it "should respond to post request with no data with an error, asking for data" do
+				post '/api/nr_mailer'
+	
+				last_response.status.should eq(400)
+				last_response.content_type.should eq(json_format)
+				last_response.body.should eq( error_400 )
+			end
+
+			it "return an error if the data sent in the body is not JSON data" do
+				post '/api/nr_mailer', 'yo dawg whats up'
+
+				last_response.status.should eq(400)
+				last_response.body.should eq( error_400 )
+			end
+
 		end
 
-		it "should respond to post request with no data with an error, asking for data" do
-			
-			post '/api/nr_mailer'
+		context "has a correctly formated request" do
 
-			last_response.status.should eq(400)
-			last_response.content_type.should eq(json_format)
-			last_response.body.should eq( { errors: [{ message: 'This resource requires you post JSON data with the "to", "subject", and "body" attributes.' }] }.to_json )
+			it "should recieve JSON data" do
+				post '/api/nr_mailer', demo_json
+				last_response.should be_ok
+			end
+
+			it "should send an email according to the data sent in json" do
+				pending
+			end
 
 		end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,12 @@ require 'rack/test'
 require 'email_spec'
 
 RSpec.configure do |config|
-  config.include Rack::Test::Methods
-  def app
-    Sinatra::Application
-  end
+	config.include Rack::Test::Methods
+	config.include EmailSpec::Helpers
+	config.include EmailSpec::Matchers
+
+	def app
+	  Sinatra::Application
+	end
+	
 end
